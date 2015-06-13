7d1d17df4098d7f7e20b19997a7a18cfed42ebd2
Kosuke Sakai <kosuke.s.sakai@gmail.com>
2015-06-13 23:36:57 +0700

    solved 4

--- 4/ruby/4.rb
+++ 4/ruby/4.rb
@@ -1,5 +1,13 @@
 require_relative '../../lib/euler.rb'
 
-answer = 0
+min = 100
+max = 999
+
+#3桁の数字の積で表せる数字のリスト
+three_digits_product = (min..max).to_a.map do |upper_limit|
+  (min..upper_limit).to_a.map{ |n| upper_limit * n }
+end.flatten.uniq
+
+answer = three_digits_product.select{ |n| n.to_s == n.to_s.reverse }.max
 
 puts answer
