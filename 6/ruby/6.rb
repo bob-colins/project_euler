8a0803616c888ba7631b3db0934da2b7c8af5624
Kosuke Sakai <kosuke.s.sakai@gmail.com>
2015-06-14 11:32:50 +0700

    solved 6

--- 6/ruby/6.rb
+++ 6/ruby/6.rb
@@ -1,5 +1,9 @@
 require_relative '../../lib/euler.rb'
 
-answer = 0
+n=100
+sum_of_squares = (1..n).to_a.map{|n| n**2}.inject(:+)
+square_of_sum = (1..n).to_a.inject(:+)**2
+
+answer = square_of_sum - sum_of_squares
 
 puts answer
